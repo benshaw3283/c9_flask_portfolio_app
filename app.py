ecacd36449f573de8ac4c5afd41ea60854945a50
benshaw3283 <38153391+benshaw3283@users.noreply.github.com>
2018-04-16 23:34:23 +1000

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = str(name) + "is a gimp"
+	new_name = str(name) + " is a gimp"
 	return render_template('index.html', name=new_name)
 
 
